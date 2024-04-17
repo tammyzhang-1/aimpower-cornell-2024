ad4271a4a844d73efb504d7692f4a26b35d7b978
Aditiramprasad <74909479+aditi-1402@users.noreply.github.com>
2024-04-17 22:08:54 +0000

    Added base fixtures

--- src/main.js
+++ src/main.js
@@ -10,10 +10,12 @@ import {createBootstrap} from 'bootstrap-vue-next'
 // Add the necessary CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-
-
+import fixtures from './fixtures.json'
+ 
 
 const app = createApp(App)
+app.config.globalProperties.fixtures = fixtures;
+app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
 
 app.use(router)
 app.use(createBootstrap({components: true, directives: true})) // Change this line
