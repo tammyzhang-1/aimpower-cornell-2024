2109cf95ac751b000106355026a5bfa46f7f5e43
Aditiramprasad <74909479+aditi-1402@users.noreply.github.com>
2024-04-17 22:06:11 +0000

    Bootstrap initialisation

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// main.js/ts
+import {createApp} from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
+
+// Add the necessary CSS
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(createBootstrap({components: true, directives: true})) // Change this line
 app.mount('#app')
