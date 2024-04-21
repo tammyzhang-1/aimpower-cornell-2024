432fb05541c40a9dcf556c1062115937fce434ed
Tammy Zhang <tz332@cornell.edu>
2024-04-20 22:40:58 -0400

    reinit to use primevue

--- src/main.js
+++ src/main.js
@@ -3,20 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// main.js/ts
-import {createApp} from 'vue'
-import {createBootstrap} from 'bootstrap-vue-next'
-
-// Add the necessary CSS
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-import fixtures from './fixtures.json'
- 
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
 const app = createApp(App)
-app.config.globalProperties.fixtures = fixtures;
-app.config.globalProperties.baseDir = import.meta.env.BASE_URL;
 
-app.use(router)
-app.use(createBootstrap({components: true, directives: true})) // Change this line
+app.use(PrimeVue)
+    .use(router);
+
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Calendar from 'primevue/calendar';
+
+// if we want to set a particular date as default:
+// const date = defineModel({ default: "01/01/24" })
+
+import { ref } from "vue";
+const date = ref();
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="card flex justify-content-center">
+        <Calendar v-model="date" inline showWeek />
+    </div>
   </main>
 </template>
