b3f654bd5207fdf9c557d313a52e2c148423c7b3
Tammy Zhang <tz332@cornell.edu>
2024-04-28 23:41:00 -0400

    style no discussion component

--- src/components/NoDiscussion.vue
+++ src/components/NoDiscussion.vue
@@ -3,23 +3,57 @@
 </script>
 
 <template>
-    <div id="no-discussion" class="test">
-        <p>Dicussion has not started yet.</p>
-        <p>Share your thoughts.</p>
-        <p>Other meeting participants are eager to hear from you!</p>
+    <div id="no-discussion-container" class="test">
+        <div id="no-discussion">
+            <img id="no-discussion-img" src="../../public/no-discussion-img.png" />
+            <p>Dicussion has not started yet.</p>
+            <p class="reduced-margin-p">Share your thoughts.</p>
+            <p class="reduced-margin-p">Other meeting participants are eager to hear from you!</p>
+        </div>
     </div>
   
 </template>
 
 <style scoped>
+    #no-discussion-container {
+        width: 100%;
+        height: 100%;
+        padding-left: 2em;
+        padding-right: 2em;
+    }
+
     #no-discussion {
         width: 100%;
-        height: 80%;
+        height: 100%;
         text-align: center;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        background-color: #F9F9F9;
+        border-radius: 11.25px;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    }
+
+    #no-discussion p {
+        color: var(--Meta-Meta, #677E92);
+        text-align: center;
+        font-size: 32px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 120%;
+    }
+    .reduced-margin-p {
+        margin-top: 0;
+        margin-bottom: 0;
+    
+    }
+
+    #no-discussion-img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 20%;
     }
 
     .test {
